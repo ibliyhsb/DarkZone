6ec1da93b3b707402b6edeab3803f041b7112edd
ibliyhsb <catd.rojas@duocuc.cl>
2025-11-15 03:13:04 -0300

    HU13-Pantalla de carga

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,6 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.compose.material:material-icons-extended")
 
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 }
